cf586a24aa5e13151d51743b38745d4f47ee3551
Tomasz Foster <tomaszfoster@gmail.com>
2017-12-21 16:06:51 -0500

    Updated transform functions

--- hw1-linux_osx/Transform.cpp
+++ hw1-linux_osx/Transform.cpp
@@ -8,29 +8,10 @@
 
 // Helper rotation function.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
-	// YOUR CODE FOR HW1 HERE
-
-	// convert degrees to radians
-	const float radians = degrees * pi / 180.0;
-
-	// create 3x3 identity matrix 
-	glm::mat3 identity(1.0f);
-
-	// there are three terms to the Rodrigues Rotation Formula
-	// and we'll break them up here into their separate parts
-	// and add them back up at the end
-	const glm::mat3 part1 = cos(radians) * identity;
 	
-	// this is the tricky one, make sure the variables
-	// are in column-major order!
-	/**
-	 *	| a b c | 
-	 * 	| d e f |
-	 * 	| g h i |
-	 * 
-	 * should be input as mat3( a, d, g, b, e, h, c, f, i);
-	 * 
-	 */	
+    const float radians = degrees * pi / 180.0;
+	
+    const glm::mat3 part1 = cos(radians) * glm::mat3(1.0f);
 
 	const glm::mat3 part2 = (1 - cos(radians)) * glm::mat3(
 		axis.x * axis.x , 	axis.x * axis.y,	axis.x * axis.z,
@@ -44,77 +25,34 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
 		axis.y,		-axis.x, 	0
 	); 
 
-	// add up the three terms and return the result
 	glm::mat3 result = part1 + part2 + part3;
 	
-	// You will change this return call
 	return result;
 }
 
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
-	// YOUR CODE FOR HW1 HERE
-	
-	// set the rotation axis to a normalized Y axis as the up vector
 	const vec3 rotationAxis = glm::normalize(up);
-	
-	// move the eye vector to a vector rotated about the Y axis,
-	// then multiply by the original eye vector to get the 
-	// correct distance from the teapot
 	eye = Transform::rotate(degrees, rotationAxis) * eye;
-	// up vector doesn't need to change at all since we rotate about it
 }
 
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
-	// YOUR CODE FOR HW1 HERE 
 
-	// set the rotation axis to a normalized right vector (eye _cross_ up is right vector)
 	const vec3 rotationAxis = glm::normalize( glm::cross(eye, up));
-	
-	// move the eye vector around our rotation axis then multiply by original
-	// to get correct distance from the teapot
-	eye = Transform::rotate(degrees, rotationAxis) * eye;
 
-	// our up axis has changed after rotating
-	// let's reset the up vector 
+	eye = Transform::rotate(degrees, rotationAxis) * eye;
 	up = glm::normalize( Transform::rotate( degrees, rotationAxis) * up );
 }
 
 // Your implementation of the glm::lookAt matrix
 mat4 Transform::lookAt(vec3 eye, vec3 up) {
-	// YOUR CODE FOR HW1 HERE
 
-	// lets define the center for readiability
-	const vec3 center = vec3(0.0, 0.0, 0.0);
-
-	// get the vector `a` which points from the `eye`, to the center of the object
-	// in this case the object lies at the origin
-	// and the camera always faces (0,0,-1)
-	// const vec3 a = vec3( eye.x - center.x , eye.y - center.y, eye.z - center.z );
-	// but more simply, since we know everything is at 0,0,0, we can just take the eye coordinates
-
-	//  get length of `a` divided by it's magnitude
-	// const vec3 w = eye / glm::length( eye );
 	const vec3 w = glm::normalize(eye);
-	// alternatively we can just normalize vector `a` to get the same result
-	const vec3 w2 = glm::normalize(vec3(
-		center.x - eye.x, center.y - eye.y, center.z - eye.z
-	)); 
 
-	// u = ( b x w / magnitude ( b x w ) )
-	// we should first make `b` a unit vector by normalizing it
-	const vec3 b = up / glm::length(up);
-	// then normalize the cross product of `b` x `w`
-	const vec3 u = glm::cross( b, w ) / glm::length( glm::cross( b, w ) ) ;
-	// alternatively, since `w` is already a unit vector, we can get the
-	// cross product of `w` and the normalized `up` vector in one line
-	// const vec3 u = glm::cross( w, glm::normalize(up) );
-
-	// lastly, to find v, we get the crossproduct of `w` x `u`
+	const vec3 u = glm::cross( glm::normalize(up), w );
 	const vec3 v = glm::cross( w, u);
 
-	// putting it all together in column-major order
 	const glm::mat4 result = glm::mat4(
 		u.x, v.x, w.x, 0.0,
 		u.y, v.y, w.y, 0.0,
